3fffc17f64b39279928e080ee3c4f7c0e02ad2c0
Hirose Shigemoto <hirose@tenda.co.jp>
2014-11-13 16:11:50 +0900

    #1 開発環境では常にエラーを出力

--- public/index.php
+++ public/index.php
@@ -3,6 +3,11 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+if ($_SERVER['APPLICATION_ENV'] == 'development') {
+    error_reporting(-1);
+    ini_set("display_errors", true);
+}
+
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
